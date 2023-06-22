36031c0f213979873c3efa05267cc64de2f99341
alenjohn05 <alenjohnmj@gmail.com>
2023-06-22 14:30:00 +0530

    Added eslintignore

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,16 +1,23 @@
- 
-import Button from './button'
-import Checkbox from './checkbox'
-import List from './list'
-import Modal from './modal'
-import Select, { SelectDataSourceItem } from './select'
-import Input from './input'
-import Loading from './loading'
-import Switch from './switch'
-import Footer from './footer'
+import Button from './button';
+import Checkbox from './checkbox';
+import List from './list';
+import Modal from './modal';
+import Select, { SelectDataSourceItem } from './select';
+import Input from './input';
+import Loading from './loading';
+import Switch from './switch';
+import Footer from './footer';
 
 export {
-  Button, Checkbox, List, Modal, Select, Input, Loading, Switch,Footer
-}
+  Button,
+  Checkbox,
+  List,
+  Modal,
+  Select,
+  Input,
+  Loading,
+  Switch,
+  Footer,
+};
 
-export type { SelectDataSourceItem }
+export type { SelectDataSourceItem };
