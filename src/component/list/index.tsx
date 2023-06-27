eeeb2654884cc0642a67cbd16404d83c64eb6f0d
alenjohn05 <alenjohnmj@gmail.com>
2023-06-27 14:30:00 +0530

    Added eslintignore

--- src/component/list/index.tsx
+++ src/component/list/index.tsx
@@ -1,42 +1,33 @@
- 
-import { ParentComponent, ParentProps, JSX, Show } from 'solid-js'
+import { ParentComponent, ParentProps, JSX, Show } from 'solid-js';
 
-import Loading from '../loading'
-import Empty from '../empty'
+import Loading from '../loading';
+import Empty from '../empty';
 
 export interface ListProps extends ParentProps {
-  class?: string
-  style?: JSX.CSSProperties | string
-  loading?: boolean
-  dataSource?: any[]
-  renderItem?: (data: any) => JSX.Element
+  class?: string;
+  style?: JSX.CSSProperties | string;
+  loading?: boolean;
+  dataSource?: any[];
+  renderItem?: (data: any) => JSX.Element;
 }
 
-const List: ParentComponent<ListProps> = props => {
+const List: ParentComponent<ListProps> = (props) => {
   return (
-    <ul
-      style={props.style}
-      class={`klinecharts-pro-list ${props.class ?? ''}`}>
+    <ul style={props.style} class={`klinecharts-pro-list ${props.class ?? ''}`}>
       <Show when={props.loading}>
-        <Loading/>
-      </Show>
-      <Show when={!props.loading && !props.children && !props.dataSource?.length}>
-        <Empty/>
+        <Loading />
       </Show>
       <Show
-        when={props.children}>
-        {props.children}
+        when={!props.loading && !props.children && !props.dataSource?.length}
+      >
+        <Empty />
       </Show>
-      <Show
-        when={!props.children}>
-        {
-          props.dataSource?.map(data => (
-            props.renderItem?.(data) ?? <li></li>
-          ))
-        }
+      <Show when={props.children}>{props.children}</Show>
+      <Show when={!props.children}>
+        {props.dataSource?.map((data) => props.renderItem?.(data) ?? <li></li>)}
       </Show>
     </ul>
-  )
-}
+  );
+};
 
-export default List
+export default List;
