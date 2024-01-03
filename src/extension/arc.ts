6ff053bc1378b7c52b9f2f6a32ae342839e650a2
alenjohn05 <alenjohnmj@gmail.com>
2024-01-03 14:30:00 +0530

    Added implememation of 00345

--- src/extension/arc.ts
+++ src/extension/arc.ts
@@ -1,5 +1,4 @@
-
-import { OverlayTemplate, utils } from  'equicharts'
+import { OverlayTemplate, utils } from 'equicharts';
 
 const arc: OverlayTemplate = {
   name: 'arc',
@@ -9,32 +8,37 @@ const arc: OverlayTemplate = {
   needDefaultYAxisFigure: true,
   styles: {
     arc: {
-      color: 'rgba(22, 119, 255)'
-    }
+      color: 'rgba(22, 119, 255)',
+    },
   },
   createPointFigures: ({ coordinates }) => {
     if (coordinates.length > 1) {
       const midX = (coordinates[0].x + coordinates[1].x) / 2;
       const midY = (coordinates[0].y + coordinates[1].y) / 2;
-      const radius = Math.sqrt(Math.pow(coordinates[1].x - coordinates[0].x, 2) + Math.pow(coordinates[1].y - coordinates[0].y, 2)) / 2;
+      const radius =
+        Math.sqrt(
+          Math.pow(coordinates[1].x - coordinates[0].x, 2) +
+            Math.pow(coordinates[1].y - coordinates[0].y, 2),
+        ) / 2;
       const startAngle = 0;
       const endAngle = Math.PI;
-      return [{
-        type: 'arc',
-        attrs: {
-          x: midX,
-          y: midY,
-          r: radius,
-          startAngle: startAngle,
-          endAngle: endAngle
+      return [
+        {
+          type: 'arc',
+          attrs: {
+            x: midX,
+            y: midY,
+            r: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+          },
+          styles: { style: 'solid' },
         },
-        styles: { style: 'solid' }
-      }];
+      ];
     }
 
     return [];
-  }
+  },
+};
 
-}
-
-export default arc
+export default arc;
