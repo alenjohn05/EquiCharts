ad0216497f52f9d68e4539f3adfb3935d53414c9
alenjohn05 <alenjohnmj@gmail.com>
2024-01-05 14:30:00 +0530

    Added implememation of 00345

--- src/extension/fibonacciDiagonal.ts
+++ src/extension/fibonacciDiagonal.ts
@@ -1,8 +1,11 @@
-
-import { OverlayTemplate, CircleAttrs, TextAttrs, LineAttrs, Coordinate } from  'equicharts'
-import { formatThousands } from './utils'
-
-
+import {
+  OverlayTemplate,
+  CircleAttrs,
+  TextAttrs,
+  LineAttrs,
+  Coordinate,
+} from 'equicharts';
+import { formatThousands } from './utils';
 
 const fibonacciDiagonal: OverlayTemplate = {
   name: 'fibonacciDiagonal',
@@ -10,49 +13,78 @@ const fibonacciDiagonal: OverlayTemplate = {
   needDefaultPointFigure: true,
   needDefaultXAxisFigure: true,
   needDefaultYAxisFigure: true,
-  createPointFigures: ({ coordinates, bounding, overlay, precision, thousandsSeparator }) => {
-    const points = overlay.points
+  createPointFigures: ({
+    coordinates,
+    bounding,
+    overlay,
+    precision,
+    thousandsSeparator,
+  }) => {
+    const points = overlay.points;
     if (coordinates.length > 0) {
-      const lines: LineAttrs[] = []
-      const texts: TextAttrs[] = []
+      const lines: LineAttrs[] = [];
+      const texts: TextAttrs[] = [];
       // const startX = 0
       // const endX = bounding.width
-      const startX = coordinates[0].x
-      const endX = coordinates[coordinates.length - 1].x
-      if (coordinates.length > 1 && points[0].value !== undefined && points[1].value !== undefined) {
-        const percents = [1, 0.786, 0.618, 0.5, 0.382, 0.236, 0]
-        const yDif = coordinates[0].y - coordinates[1].y
-        const valueDif = points[0].value - points[1].value
-        percents.forEach(percent => {
-          const y = coordinates[1].y + yDif * percent
-          const value = formatThousands(((points[1].value ?? 0) + valueDif * percent).toFixed(precision.price), thousandsSeparator)
-          lines.push({ coordinates: [{ x: startX, y }, { x: endX, y }] })
+      const startX = coordinates[0].x;
+      const endX = coordinates[coordinates.length - 1].x;
+      if (
+        coordinates.length > 1 &&
+        points[0].value !== undefined &&
+        points[1].value !== undefined
+      ) {
+        const percents = [1, 0.786, 0.618, 0.5, 0.382, 0.236, 0];
+        const yDif = coordinates[0].y - coordinates[1].y;
+        const valueDif = points[0].value - points[1].value;
+        percents.forEach((percent) => {
+          const y = coordinates[1].y + yDif * percent;
+          const value = formatThousands(
+            ((points[1].value ?? 0) + valueDif * percent).toFixed(
+              precision.price,
+            ),
+            thousandsSeparator,
+          );
+          lines.push({
+            coordinates: [
+              { x: startX, y },
+              { x: endX, y },
+            ],
+          });
           texts.push({
             x: startX,
             y,
             text: `${value} (${(percent * 100).toFixed(1)}%)`,
-            baseline: 'bottom'
-          })
-        })
-        lines.push({ coordinates: [{ x: lines[0].coordinates[0].x, y: lines[0].coordinates[0].y }, { x: lines[percents.length-1].coordinates[1].x, y: lines[percents.length-1].coordinates[1].y }]})
+            baseline: 'bottom',
+          });
+        });
+        lines.push({
+          coordinates: [
+            { x: lines[0].coordinates[0].x, y: lines[0].coordinates[0].y },
+            {
+              x: lines[percents.length - 1].coordinates[1].x,
+              y: lines[percents.length - 1].coordinates[1].y,
+            },
+          ],
+        });
       }
       return [
         {
           type: 'line',
-          attrs: lines
-        }, {
+          attrs: lines,
+        },
+        {
           type: 'text',
           isCheckEvent: false,
-          attrs: texts
-        }
-      ]
+          attrs: texts,
+        },
+      ];
     }
-    return []
+    return [];
   },
   onRightClick: (event): boolean => {
-    alert(`object ${event.overlay.name} was clicked`)
+    alert(`object ${event.overlay.name} was clicked`);
     return true;
-  }
-}
+  },
+};
 
-export default fibonacciDiagonal
+export default fibonacciDiagonal;
