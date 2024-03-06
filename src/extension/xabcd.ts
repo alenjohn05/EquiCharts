ea97ae9de8f94065299d5ff75cee2025d35423a7
alenjohn05 <alenjohnmj@gmail.com>
2024-03-06 14:30:00 +0530

    Added implememation of 00345

--- src/extension/xabcd.ts
+++ src/extension/xabcd.ts
@@ -1,5 +1,4 @@
- 
-import { OverlayTemplate, PolygonAttrs, LineAttrs } from  'equicharts'
+import { OverlayTemplate, PolygonAttrs, LineAttrs } from 'equicharts';
 
 const xabcd: OverlayTemplate = {
   name: 'xabcd',
@@ -9,51 +8,55 @@ const xabcd: OverlayTemplate = {
   needDefaultYAxisFigure: true,
   styles: {
     polygon: {
-      color: 'rgba(22, 119, 255, 0.15)'
-    }
+      color: 'rgba(22, 119, 255, 0.15)',
+    },
   },
   createPointFigures: ({ coordinates, overlay }) => {
-    const dashedLines: LineAttrs[] = []
-    const polygons: PolygonAttrs[] = []
-    const tags = ['X', 'A', 'B', 'C', 'D']
+    const dashedLines: LineAttrs[] = [];
+    const polygons: PolygonAttrs[] = [];
+    const tags = ['X', 'A', 'B', 'C', 'D'];
     const texts = coordinates.map((coordinate, i) => ({
       ...coordinate,
       baseline: 'bottom',
-      text: `(${tags[i]})`
-    }))
+      text: `(${tags[i]})`,
+    }));
     if (coordinates.length > 2) {
-      dashedLines.push({ coordinates: [coordinates[0], coordinates[2]] })
-      polygons.push({ coordinates: [coordinates[0], coordinates[1], coordinates[2]] })
+      dashedLines.push({ coordinates: [coordinates[0], coordinates[2]] });
+      polygons.push({
+        coordinates: [coordinates[0], coordinates[1], coordinates[2]],
+      });
       if (coordinates.length > 3) {
-        dashedLines.push({ coordinates: [coordinates[1], coordinates[3]] })
+        dashedLines.push({ coordinates: [coordinates[1], coordinates[3]] });
         if (coordinates.length > 4) {
-          dashedLines.push({ coordinates: [coordinates[2], coordinates[4]] })
-          polygons.push({ coordinates: [coordinates[2], coordinates[3], coordinates[4]] })
+          dashedLines.push({ coordinates: [coordinates[2], coordinates[4]] });
+          polygons.push({
+            coordinates: [coordinates[2], coordinates[3], coordinates[4]],
+          });
         }
       }
     }
     return [
       {
         type: 'line',
-        attrs: { coordinates }
+        attrs: { coordinates },
       },
       {
         type: 'line',
         attrs: dashedLines,
-        styles: { style: 'dashed' }
+        styles: { style: 'dashed' },
       },
       {
         type: 'polygon',
         ignoreEvent: true,
-        attrs: polygons
+        attrs: polygons,
       },
       {
         type: 'text',
         ignoreEvent: true,
-        attrs: texts
-      }
-    ]
-  }
-}
+        attrs: texts,
+      },
+    ];
+  },
+};
 
-export default xabcd
+export default xabcd;
