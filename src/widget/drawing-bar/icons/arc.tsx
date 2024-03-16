ed46cedd0928820649226542115ef80c8b5489e0
alenjohn05 <alenjohnmj@gmail.com>
2024-03-16 14:30:00 +0530

    Added implememation of 00345

--- src/widget/drawing-bar/icons/arc.tsx
+++ src/widget/drawing-bar/icons/arc.tsx
@@ -1,10 +1,50 @@
-
 export default () => (
   <svg class="icon-overlay" viewBox="0 0 22 22">
-    <ellipse cx="10.5" cy="11.5" rx="1.5" ry="1.5" stroke-opacity="0" stroke="none"/>
-    <ellipse cx="17.5" cy="11.5" rx="1.5" ry="1.5" stroke-opacity="0" stroke="none"/>
-    <ellipse cx="10.5" cy="11.5" rx="7" ry="7" fill-opacity="0" stroke-opacity="1" fill="none" stroke-width="1"/>
-    <ellipse cx="10.5" cy="11.5" rx="5" ry="5" fill-opacity="0" stroke-opacity="1" fill="none" stroke-width="1"/>
-    <ellipse cx="10.5" cy="11.5" rx="3" ry="3" fill-opacity="0" stroke-opacity="1" fill="none" stroke-width="1"/>
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="1.5"
+      ry="1.5"
+      stroke-opacity="0"
+      stroke="none"
+    />
+    <ellipse
+      cx="17.5"
+      cy="11.5"
+      rx="1.5"
+      ry="1.5"
+      stroke-opacity="0"
+      stroke="none"
+    />
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="7"
+      ry="7"
+      fill-opacity="0"
+      stroke-opacity="1"
+      fill="none"
+      stroke-width="1"
+    />
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="5"
+      ry="5"
+      fill-opacity="0"
+      stroke-opacity="1"
+      fill="none"
+      stroke-width="1"
+    />
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="3"
+      ry="3"
+      fill-opacity="0"
+      stroke-opacity="1"
+      fill="none"
+      stroke-width="1"
+    />
   </svg>
-)
+);
