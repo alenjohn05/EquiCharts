8abf09bf0e091f2668e8c963906352f8b3188248
alenjohn05 <alenjohnmj@gmail.com>
2024-03-18 14:30:00 +0530

    Added implememation of 00345

--- src/widget/drawing-bar/icons/circle.tsx
+++ src/widget/drawing-bar/icons/circle.tsx
@@ -1,8 +1,30 @@
- 
 export default () => (
   <svg class="icon-overlay" viewBox="0 0 22 22">
-    <ellipse cx="10.5" cy="11.5" rx="1.5" ry="1.5" stroke-opacity="0" stroke="none"/>
-    <ellipse cx="17.5" cy="11.5" rx="1.5" ry="1.5" stroke-opacity="0" stroke="none"/>
-    <ellipse cx="10.5" cy="11.5" rx="7" ry="7" fill-opacity="0" fill="none" stroke-opacity="1" stroke-width="1"/>
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="1.5"
+      ry="1.5"
+      stroke-opacity="0"
+      stroke="none"
+    />
+    <ellipse
+      cx="17.5"
+      cy="11.5"
+      rx="1.5"
+      ry="1.5"
+      stroke-opacity="0"
+      stroke="none"
+    />
+    <ellipse
+      cx="10.5"
+      cy="11.5"
+      rx="7"
+      ry="7"
+      fill-opacity="0"
+      fill="none"
+      stroke-opacity="1"
+      stroke-width="1"
+    />
   </svg>
-)
+);
