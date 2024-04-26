0019a22c7cbfc0c95ceafd61e4078ad8f3edf6ab
alenjohn05 <alenjohnmj@gmail.com>
2024-04-26 14:30:00 +0530

    Added implememation of 00348

--- src/widget/indicator-modal/index.tsx
+++ src/widget/indicator-modal/index.tsx
@@ -1,78 +1,107 @@
- 
-import { Component, createMemo } from 'solid-js'
+import { Component, createMemo } from 'solid-js';
 
-import { Modal, List, Checkbox } from '../../component'
+import { Modal, List, Checkbox } from '../../component';
 
-import i18n from '../../i18n'
+import i18n from '../../i18n';
 
-type OnIndicatorChange = (
-  params: {
-    name: string
-    paneId: string
-    added: boolean
-  }
-) => void
+type OnIndicatorChange = (params: {
+  name: string;
+  paneId: string;
+  added: boolean;
+}) => void;
 
 export interface IndicatorModalProps {
-  locale: string
-  mainIndicators: string[]
-  subIndicators: object
-  onMainIndicatorChange: OnIndicatorChange
-  onSubIndicatorChange: OnIndicatorChange
-  onClose: () => void
+  locale: string;
+  mainIndicators: string[];
+  subIndicators: object;
+  onMainIndicatorChange: OnIndicatorChange;
+  onSubIndicatorChange: OnIndicatorChange;
+  onClose: () => void;
 }
 
-const IndicatorModal: Component<IndicatorModalProps> = props => {
-
+const IndicatorModal: Component<IndicatorModalProps> = (props) => {
   return (
     <Modal
       title={i18n('indicator', props.locale)}
       width={400}
-      onClose={props.onClose}>
-      <List
-        class="klinecharts-pro-indicator-modal-list">
+      onClose={props.onClose}
+    >
+      <List class="klinecharts-pro-indicator-modal-list">
         <li class="title">{i18n('main_indicator', props.locale)}</li>
-        {
-          [
-            'MA', 'EMA', 'SMA', 'BOLL', 'SAR', 'BBI', 'candle_volume'
-          ].map(name => {
-            const checked = props.mainIndicators.includes(name)
+        {['MA', 'EMA', 'SMA', 'BOLL', 'SAR', 'BBI', 'candle_volume'].map(
+          (name) => {
+            const checked = props.mainIndicators.includes(name);
             return (
               <li
                 class="row"
-                onClick={_ => {
-                  props.onMainIndicatorChange({ name, paneId: 'candle_pane', added: !checked })
-                }}>
-                <Checkbox checked={checked} label={i18n(name.toLowerCase(), props.locale)}/>
+                onClick={(_) => {
+                  props.onMainIndicatorChange({
+                    name,
+                    paneId: 'candle_pane',
+                    added: !checked,
+                  });
+                }}
+              >
+                <Checkbox
+                  checked={checked}
+                  label={i18n(name.toLowerCase(), props.locale)}
+                />
               </li>
-            )
-          })
-        }
+            );
+          },
+        )}
         <li class="title">{i18n('sub_indicator', props.locale)}</li>
-        {
-          [
-            'MA', 'EMA', 'VOL', 'MACD', 'BOLL', 'KDJ',
-            'RSI', 'BIAS', 'BRAR', 'CCI', 'DMI',
-            'CR', 'PSY', 'DMA', 'TRIX', 'OBV',
-            'VR', 'WR', 'MTM', 'EMV', 'SAR',
-            'SMA', 'ROC', 'PVT', 'BBI', 'AO'
-          ].map(name => {
-            const checked = name in props.subIndicators
-            return (
-              <li
-                class="row"
-                onClick={_ => {
-                  // @ts-expect-error
-                  props.onSubIndicatorChange({ name, paneId: props.subIndicators[name] ?? '', added: !checked });
-                }}>
-                <Checkbox checked={checked} label={i18n(name.toLowerCase(), props.locale)}/>
-              </li>
-            )
-          })
-        }
+        {[
+          'MA',
+          'EMA',
+          'VOL',
+          'MACD',
+          'BOLL',
+          'KDJ',
+          'RSI',
+          'BIAS',
+          'BRAR',
+          'CCI',
+          'DMI',
+          'CR',
+          'PSY',
+          'DMA',
+          'TRIX',
+          'OBV',
+          'VR',
+          'WR',
+          'MTM',
+          'EMV',
+          'SAR',
+          'SMA',
+          'ROC',
+          'PVT',
+          'BBI',
+          'AO',
+        ].map((name) => {
+          const checked = name in props.subIndicators;
+          return (
+            <li
+              class="row"
+              onClick={(_) => {
+                // @ts-expect-error
+                props.onSubIndicatorChange({
+                  name,
+                  paneId: props.subIndicators[name] ?? '',
+                  added: !checked,
+                });
+              }}
+            >
+              <Checkbox
+                checked={checked}
+                label={i18n(name.toLowerCase(), props.locale)}
+              />
+            </li>
+          );
+        })}
       </List>
     </Modal>
-  )
-}
+  );
+};
 
-export default IndicatorModal
+export default IndicatorModal;
