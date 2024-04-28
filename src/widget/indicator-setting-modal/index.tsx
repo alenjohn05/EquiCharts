743d3f3b5ba14f3a6ed3ed4a8921168d16f87f21
alenjohn05 <alenjohnmj@gmail.com>
2024-04-28 14:30:00 +0530

    Added implememation of 00348

--- src/widget/indicator-setting-modal/index.tsx
+++ src/widget/indicator-setting-modal/index.tsx
@@ -1,28 +1,31 @@
- 
-import { Component, createSignal } from 'solid-js'
+import { Component, createSignal } from 'solid-js';
 
-import { utils } from  'equicharts'
+import { utils } from 'equicharts';
 
-import { Modal, Input } from '../../component'
+import { Modal, Input } from '../../component';
 
-import i18n from '../../i18n'
+import i18n from '../../i18n';
 
-import data from './data'
+import data from './data';
 
 export interface IndicatorSettingModalProps {
-  locale: string
-  params: { indicatorName: string, paneId: string, calcParams: any[] }
-  onClose: () => void
-  onConfirm: (calcParams: any) => void
+  locale: string;
+  params: { indicatorName: string; paneId: string; calcParams: any[] };
+  onClose: () => void;
+  onConfirm: (calcParams: any) => void;
 }
 
-const IndicatorSettingModal: Component<IndicatorSettingModalProps> = props => {
-  const [calcParams, setCalcParams] = createSignal(utils.clone(props.params.calcParams))
+const IndicatorSettingModal: Component<IndicatorSettingModalProps> = (
+  props,
+) => {
+  const [calcParams, setCalcParams] = createSignal(
+    utils.clone(props.params.calcParams),
+  );
 
   const getConfig: (name: string) => any[] = (name: string) => {
     // @ts-expect-error
-    return data[name]
-  }
+    return data[name];
+  };
 
   return (
     <Modal
@@ -33,47 +36,46 @@ const IndicatorSettingModal: Component<IndicatorSettingModalProps> = props => {
           type: 'confirm',
           children: i18n('confirm', props.locale),
           onClick: () => {
-            const config = getConfig(props.params.indicatorName)
-            const params: any[] = []
+            const config = getConfig(props.params.indicatorName);
+            const params: any[] = [];
             utils.clone(calcParams()).forEach((param: any, i: number) => {
               if (!utils.isValid(param) || param === '') {
                 if ('default' in config[i]) {
-                  params.push(config[i]['default'])
+                  params.push(config[i]['default']);
                 }
               } else {
-                params.push(param)
+                params.push(param);
               }
-            })
-            props.onConfirm(params)
-            props.onClose()
-          }
-        }
+            });
+            props.onConfirm(params);
+            props.onClose();
+          },
+        },
       ]}
-      onClose={props.onClose}>
+      onClose={props.onClose}
+    >
       <div class="klinecharts-pro-indicator-setting-modal-content">
-        {
-          getConfig(props.params.indicatorName).map((d, i) => {
-            return (
-              <>
-                <span >{i18n(d.paramNameKey, props.locale)}</span>
-                <Input
-                  style={{ width: '200px' }}
-                  value={calcParams()[i] ?? ''}
-                  precision={d.precision}
-                  min={d.min}
-                  onChange={value => {
-                    const params = utils.clone(calcParams())
-                    params[i] = value
-                    setCalcParams(params)
-                  }}/>
-              </>
-            )
-          })
-        }
+        {getConfig(props.params.indicatorName).map((d, i) => {
+          return (
+            <>
+              <span>{i18n(d.paramNameKey, props.locale)}</span>
+              <Input
+                style={{ width: '200px' }}
+                value={calcParams()[i] ?? ''}
+                precision={d.precision}
+                min={d.min}
+                onChange={(value) => {
+                  const params = utils.clone(calcParams());
+                  params[i] = value;
+                  setCalcParams(params);
+                }}
+              />
+            </>
+          );
+        })}
       </div>
-      
     </Modal>
-  )
-}
+  );
+};
 
-export default IndicatorSettingModal
+export default IndicatorSettingModal;
