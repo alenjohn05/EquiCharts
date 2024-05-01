608600510c307af90b9e4ac23152bac9298a7c14
alenjohn05 <alenjohnmj@gmail.com>
2024-05-01 14:30:00 +0530

    Added implememation of 00348

--- src/widget/screenshot-modal/index.tsx
+++ src/widget/screenshot-modal/index.tsx
@@ -1,17 +1,16 @@
- 
-import { Component } from 'solid-js'
+import { Component } from 'solid-js';
 
-import { Modal } from '../../component'
+import { Modal } from '../../component';
 
-import i18n from '../../i18n'
+import i18n from '../../i18n';
 
 export interface ScreenshotModalProps {
-  locale: string
-  url: string
-  onClose: () => void
+  locale: string;
+  url: string;
+  onClose: () => void;
 }
 
-const ScreenshotModal: Component<ScreenshotModalProps> = props => {
+const ScreenshotModal: Component<ScreenshotModalProps> = (props) => {
   return (
     <Modal
       title={i18n('screenshot', props.locale)}
@@ -21,19 +20,20 @@ const ScreenshotModal: Component<ScreenshotModalProps> = props => {
           type: 'confirm',
           children: i18n('save', props.locale),
           onClick: () => {
-            const a = document.createElement('a')
-            a.download = 'screenshot'
-            a.href = props.url
-            document.body.appendChild(a)
-            a.click()
-            a.remove()
-          }
-        }
+            const a = document.createElement('a');
+            a.download = 'screenshot';
+            a.href = props.url;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+          },
+        },
       ]}
-      onClose={props.onClose}>
-      <img style="width:500px;margin-top: 20px" src={props.url}/>
+      onClose={props.onClose}
+    >
+      <img style="width:500px;margin-top: 20px" src={props.url} />
     </Modal>
-  )
-}
+  );
+};
 
-export default ScreenshotModal
+export default ScreenshotModal;
