9acdba8cf38ee5f60c0f39e74b9abde6bc97cf7b
alenjohn05 <alenjohnmj@gmail.com>
2024-05-01 14:30:00 +0530

    Added implememation of 00348

--- src/widget/period-bar/candle_icons/ohlc.tsx
+++ src/widget/period-bar/candle_icons/ohlc.tsx
@@ -1,4 +1,8 @@
- 
 export default () => (
-  <svg class="icon-overlay" viewBox="0 0 28 28" width="28" height="28"><path fill="currentColor" d="M19 6h-1v7h-3v1h3v8h1v-3h3v-1h-3V6ZM11 7h-1v13H7v1h3v2h1V10h3V9h-3V7Z"></path></svg>
-)
+  <svg class="icon-overlay" viewBox="0 0 28 28" width="28" height="28">
+    <path
+      fill="currentColor"
+      d="M19 6h-1v7h-3v1h3v8h1v-3h3v-1h-3V6ZM11 7h-1v13H7v1h3v2h1V10h3V9h-3V7Z"
+    ></path>
+  </svg>
+);
