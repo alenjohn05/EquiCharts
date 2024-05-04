e489f005c5e03c26423e42a446bc8c4f1cad7303
alenjohn05 <alenjohnmj@gmail.com>
2024-05-04 14:30:00 +0530

    Added implememation of 00348

--- src/widget/timezone-modal/data.ts
+++ src/widget/timezone-modal/data.ts
@@ -1,33 +1,52 @@
- 
-import i18n from '../../i18n'
+import i18n from '../../i18n';
 
-import { SelectDataSourceItem } from '../../component'
+import { SelectDataSourceItem } from '../../component';
 
-export function translateTimezone (timezone: string, locale: string): string {
+export function translateTimezone(timezone: string, locale: string): string {
   switch (timezone) {
-    case 'Etc/UTC': return i18n('utc', locale)
-    case 'Pacific/Honolulu': return i18n('honolulu', locale)
-    case 'America/Juneau': return i18n('juneau', locale)
-    case 'America/Los_Angeles': return i18n('los_angeles', locale)
-    case 'America/Chicago': return i18n('chicago', locale)
-    case'America/Toronto': return i18n('toronto', locale)
-    case 'America/Sao_Paulo': return i18n('sao_paulo', locale)
-    case 'Europe/London': return i18n('london', locale)
-    case 'Europe/Berlin': return i18n('berlin', locale)
-    case 'Asia/Bahrain': return i18n('bahrain', locale)
-    case 'Asia/Dubai': return i18n('dubai', locale)
-    case 'Asia/Ashkhabad': return i18n('ashkhabad', locale)
-    case 'Asia/Almaty': return i18n('almaty', locale)
-    case 'Asia/Bangkok': return i18n('bangkok', locale)
-    case 'Asia/Shanghai': return i18n('shanghai', locale)
-    case 'Asia/Tokyo': return i18n('tokyo', locale)
-    case 'Australia/Sydney': return i18n('sydney', locale)
-    case 'Pacific/Norfolk': return i18n('norfolk', locale)
+    case 'Etc/UTC':
+      return i18n('utc', locale);
+    case 'Pacific/Honolulu':
+      return i18n('honolulu', locale);
+    case 'America/Juneau':
+      return i18n('juneau', locale);
+    case 'America/Los_Angeles':
+      return i18n('los_angeles', locale);
+    case 'America/Chicago':
+      return i18n('chicago', locale);
+    case 'America/Toronto':
+      return i18n('toronto', locale);
+    case 'America/Sao_Paulo':
+      return i18n('sao_paulo', locale);
+    case 'Europe/London':
+      return i18n('london', locale);
+    case 'Europe/Berlin':
+      return i18n('berlin', locale);
+    case 'Asia/Bahrain':
+      return i18n('bahrain', locale);
+    case 'Asia/Dubai':
+      return i18n('dubai', locale);
+    case 'Asia/Ashkhabad':
+      return i18n('ashkhabad', locale);
+    case 'Asia/Almaty':
+      return i18n('almaty', locale);
+    case 'Asia/Bangkok':
+      return i18n('bangkok', locale);
+    case 'Asia/Shanghai':
+      return i18n('shanghai', locale);
+    case 'Asia/Tokyo':
+      return i18n('tokyo', locale);
+    case 'Australia/Sydney':
+      return i18n('sydney', locale);
+    case 'Pacific/Norfolk':
+      return i18n('norfolk', locale);
   }
-  return timezone
+  return timezone;
 }
 
-export function createTimezoneSelectOptions (locale: string): SelectDataSourceItem[] {
+export function createTimezoneSelectOptions(
+  locale: string,
+): SelectDataSourceItem[] {
   return [
     { key: 'Etc/UTC', text: i18n('utc', locale) },
     { key: 'Pacific/Honolulu', text: i18n('honolulu', locale) },
@@ -46,6 +65,6 @@ export function createTimezoneSelectOptions (locale: string): SelectDataSourceIt
     { key: 'Asia/Shanghai', text: i18n('shanghai', locale) },
     { key: 'Asia/Tokyo', text: i18n('tokyo', locale) },
     { key: 'Australia/Sydney', text: i18n('sydney', locale) },
-    { key: 'Pacific/Norfolk', text: i18n('norfolk', locale) }
-  ]
+    { key: 'Pacific/Norfolk', text: i18n('norfolk', locale) },
+  ];
 }
