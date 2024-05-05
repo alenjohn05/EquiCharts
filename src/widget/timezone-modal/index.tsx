628a54e6bc03b824e0f9339d5ea1c72d2e5601af
alenjohn05 <alenjohnmj@gmail.com>
2024-05-05 14:30:00 +0530

    Added implememation of 00348

--- src/widget/timezone-modal/index.tsx
+++ src/widget/timezone-modal/index.tsx
@@ -1,23 +1,24 @@
- 
-import { Component, createSignal, createMemo } from 'solid-js'
+import { Component, createSignal, createMemo } from 'solid-js';
 
-import { Modal, Select } from '../../component'
-import type { SelectDataSourceItem } from '../../component'
-import i18n from '../../i18n'
+import { Modal, Select } from '../../component';
+import type { SelectDataSourceItem } from '../../component';
+import i18n from '../../i18n';
 
-import { createTimezoneSelectOptions } from './data'
+import { createTimezoneSelectOptions } from './data';
 
 export interface TimezoneModalProps {
-  locale: string
-  timezone: SelectDataSourceItem
-  onClose: () => void
-  onConfirm: (timezone: SelectDataSourceItem) => void
+  locale: string;
+  timezone: SelectDataSourceItem;
+  onClose: () => void;
+  onConfirm: (timezone: SelectDataSourceItem) => void;
 }
 
-const TimezoneModal: Component<TimezoneModalProps> = props => {
-  const [innerTimezone, setInnerTimezone] = createSignal(props.timezone)
+const TimezoneModal: Component<TimezoneModalProps> = (props) => {
+  const [innerTimezone, setInnerTimezone] = createSignal(props.timezone);
 
-  const timezoneOptions = createMemo(() => createTimezoneSelectOptions(props.locale))
+  const timezoneOptions = createMemo(() =>
+    createTimezoneSelectOptions(props.locale),
+  );
 
   return (
     <Modal
@@ -27,19 +28,23 @@ const TimezoneModal: Component<TimezoneModalProps> = props => {
         {
           children: i18n('confirm', props.locale),
           onClick: () => {
-            props.onConfirm(innerTimezone())
-            props.onClose()
-          }
-        }
+            props.onConfirm(innerTimezone());
+            props.onClose();
+          },
+        },
       ]}
-      onClose={props.onClose}>
+      onClose={props.onClose}
+    >
       <Select
         style={{ width: '100%', 'margin-top': '20px' }}
         value={innerTimezone().text}
-        onSelected={(tz) => { setInnerTimezone(tz as SelectDataSourceItem) }}
-        dataSource={timezoneOptions()}/>
+        onSelected={(tz) => {
+          setInnerTimezone(tz as SelectDataSourceItem);
+        }}
+        dataSource={timezoneOptions()}
+      />
     </Modal>
-  )
-}
+  );
+};
 
-export default TimezoneModal
+export default TimezoneModal;
